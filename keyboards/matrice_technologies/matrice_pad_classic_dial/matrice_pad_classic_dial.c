73f4b2cd64a4b0f94c5a8d9e2ad2792dadd340c8
Griffin Puc <griffinpuc@outlook.com>
2025-07-31 08:30:26 -0400

    Add Matrice Technologies keyboards

---
+++ keyboards/matrice_technologies/matrice_pad_classic_dial/matrice_pad_classic_dial.c
@@ -0,0 +1,61 @@
+/* Copyright 2025 Matrice Technologies
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+ 
+#include "quantum.h"
+#include QMK_KEYBOARD_H
+#include "print.h"
+
+void keyboard_pre_init_kb(void) {
+    // Set LED IO as outputs
+    gpio_set_pin_output(LED_00);
+    gpio_set_pin_output(LED_01);
+	gpio_set_pin_input_high(F6);
+    keyboard_pre_init_user();
+}
+
+bool shutdown_kb(bool jump_to_bootloader) {
+    if (!shutdown_user(jump_to_bootloader)) {
+        return false;
+    }
+    // Shutdown LEDs
+    gpio_write_pin_low(LED_00);
+    gpio_write_pin_low(LED_01);
+    return true;
+}
+
+layer_state_t layer_state_set_kb(layer_state_t state) {
+    // Layer LEDs act as binary indication of current layer
+    uint8_t layer = get_highest_layer(state);
+    gpio_write_pin(LED_00, layer & 0b1);
+    gpio_write_pin(LED_01, (layer >> 1) & 0b1);
+    return layer_state_set_user(state);
+}
+
+void matrix_init_kb(void) {
+    uint8_t led_delay_ms = 80;
+    for (int i = 0; i < 2; i++) {
+        gpio_write_pin_high(LED_00);
+        gpio_write_pin_high(LED_01);
+        wait_ms(led_delay_ms);
+        gpio_write_pin_low(LED_00);
+        gpio_write_pin_low(LED_01);
+        if (i < 1) {
+            wait_ms(led_delay_ms);
+        }
+    }
+
+    matrix_init_user();
+}
